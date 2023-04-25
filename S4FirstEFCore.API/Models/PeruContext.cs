6ef8f6018c887e784ac24c7b55ca7ad2541352a1
Luis Chang <luis.chang@outlook.com>
2023-04-25 08:41:11 -0500

    Agregar archivos de proyecto.

---
+++ S4FirstEFCore.API/Models/PeruContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace S4FirstEFCore.API.Models;
+
+public partial class PeruContext : DbContext
+{
+    public PeruContext()
+    {
+    }
+
+    public PeruContext(DbContextOptions<PeruContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<People> People { get; set; }
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=Peru;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<People>(entity =>
+        {
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Fullname).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ S4FirstEFCore.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace S4FirstEFCore.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
